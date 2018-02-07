4af5d36347adaae6d1dc598069837a41b4efab7c
gonka <30638707+gonkaotic@users.noreply.github.com>
2018-02-07 15:58:49 +0100

    changed a bit the format
    changed the sql1 strings format a bit to make it easier to edit it if we need it

--- src/SQLManager.java
+++ src/SQLManager.java
@@ -49,16 +49,25 @@ public abstract class SQLManager {
 
 	public static void createClientTable(Connection c) throws SQLException {
 		Statement stmt1 = c.createStatement();
-		String sql1 = "CREATE TABLE client" + "(id INT PRIMARY KEY," + "name TEXT NOT NULL," + "adress TEXT NOT NULL,"
-				+ "telephone INT," + "email TEXT," + "payment_method TEXT NOT NULL)";
+		String sql1 = "CREATE TABLE client" 
+				+ "(id INT PRIMARY KEY," 
+				+ "name TEXT NOT NULL," 
+				+ "adress TEXT NOT NULL,"
+				+ "telephone INT," 
+				+ "email TEXT," 
+				+ "payment_method TEXT NOT NULL)";
 		stmt1.executeUpdate(sql1);
 		stmt1.close();
 	}
 
 	public static void createEmployeeTable(Connection c) throws SQLException {
 		Statement stmt1 = c.createStatement();
-		String sql1 = "CREATE TABLE employee" + "(id INT PRIMARY KEY," + "name TEXT NOT NULL,"
-				+ "salary FLOAT NOT NULL," + "phone INT NOT NULL," + "position TEXT NOT NULL,"
+		String sql1 = "CREATE TABLE employee"
+				+ "(id INT PRIMARY KEY," 
+				+ "name TEXT NOT NULL,"
+				+ "salary FLOAT NOT NULL," 
+				+ "phone INT NOT NULL," 
+				+ "position TEXT NOT NULL,"
 				+ "warehouse_id REFERENCES warehouse (id))";
 		stmt1.executeUpdate(sql1);
 		stmt1.close();
@@ -66,7 +75,9 @@ public abstract class SQLManager {
 
 	public static void createCorridorTable(Connection c) throws SQLException {
 		Statement stmt1 = c.createStatement();
-		String sql1 = "CREATE TABLE corridor" + "(id INTEGER PRIMARY KEY AUTOINCREMENT," + "temperature FLOAT NOT NULL,"
+		String sql1 = "CREATE TABLE corridor" 
+				+ "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ "temperature FLOAT NOT NULL,"
 				+ "warehouse_id REFERENCES warehouse (id))";
 		stmt1.executeUpdate(sql1);
 		stmt1.close();
@@ -74,16 +85,23 @@ public abstract class SQLManager {
 
 	public static void createWarehouseTable(Connection c) throws SQLException {
 		Statement stmt1 = c.createStatement();
-		String sql1 = "CREATE TABLE warehouse" + "(id INTEGER PRIMARY KEY AUTOINCREMENT," + "phone INT NOT NULL,"
-				+ "country TEXT NOT NULL," + "address TEXT NOT NULL," + "pc INT NOT NULL)";
+		String sql1 = "CREATE TABLE warehouse"
+				+ "(id INTEGER PRIMARY KEY AUTOINCREMENT," 
+				+ "phone INT NOT NULL,"
+				+ "country TEXT NOT NULL," 
+				+ "address TEXT NOT NULL," 
+				+ "pc INT NOT NULL)";
 		stmt1.executeUpdate(sql1);
 		stmt1.close();
 	}
 
 	public static void createDeliveriesTable(Connection c) throws SQLException {
 		Statement stmt1 = c.createStatement();
-		String sql1 = "CREATE TABLE deliveries  " + "(transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-				+ " selling_price INT NOT NULL," + " amount INT NOT NULL," + " transaction_date DATE NOT NULL,"
+		String sql1 = "CREATE TABLE deliveries  "
+				+ "(transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ " selling_price INT NOT NULL,"
+				+ " amount INT NOT NULL,"
+				+ " transaction_date DATE NOT NULL,"
 				+ " client_id INT REFERENCES client (id) )";
 		stmt1.executeUpdate(sql1);
 		stmt1.close();
@@ -91,7 +109,10 @@ public abstract class SQLManager {
 
 	public static void createPackagedTable(Connection c) throws SQLException {
 		Statement stmt1 = c.createStatement();
-		String sql1 = "CREATE TABLE packaged " + "(drug_id INT," + " transaction_id INT," + " PRIMARY KEY (drug_id,"
+		String sql1 = "CREATE TABLE packaged " 
+				+ "(drug_id INT,"
+				+ " transaction_id INT,"
+				+ " PRIMARY KEY (drug_id,"
 				+ " transaction_id) )";
 		stmt1.executeUpdate(sql1);
 		stmt1.close();
@@ -99,15 +120,21 @@ public abstract class SQLManager {
 
 	public static void createDrugTable(Connection c) throws SQLException {
 		Statement stmt1 = c.createStatement();
-		String sql1 = "CREATE TABLE drug " + "(id INTEGER PRIMARY KEY AUTOINCREMENT," + " name TEXT NOT NULL UNIQUE,"
-				+ " stock INT NOT NULL," + " active_principle TEXT," + " corridor_id INT REFERENCES corridor (id) )";
+		String sql1 = "CREATE TABLE drug "
+				+ "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ " name TEXT NOT NULL UNIQUE,"
+				+ " stock INT NOT NULL,"
+				+ " active_principle TEXT,"
+				+ " corridor_id INT REFERENCES corridor (id) )";
 		stmt1.executeUpdate(sql1);
 		stmt1.close();
 	}
 
 	public static void createArrivesTable(Connection c) throws SQLException {
 		Statement stmt1 = c.createStatement();
-		String sql1 = "CREATE TABLE arrives " + "(drug_id INT," + " transaction_id INT,"
+		String sql1 = "CREATE TABLE arrives "
+				+ "(drug_id INT,"
+				+ " transaction_id INT,"
 				+ " PRIMARY KEY (drug_id, transaction_id) )";
 		stmt1.executeUpdate(sql1);
 		stmt1.close();
@@ -115,8 +142,11 @@ public abstract class SQLManager {
 
 	public static void createArrivalsTable(Connection c) throws SQLException {
 		Statement stmt1 = c.createStatement();
-		String sql1 = "CREATE TABLE arrivals " + "(transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-				+ " buying_price INT NOT NULL," + " transaction_date DATE NOT NULL," + " amount INT NOT NULL,"
+		String sql1 = "CREATE TABLE arrivals "
+				+ "(transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ " buying_price INT NOT NULL,"
+				+ " transaction_date DATE NOT NULL,"
+				+ " amount INT NOT NULL,"
 				+ " provider_id REFERENCES provider (id) )";
 		stmt1.executeUpdate(sql1);
 		stmt1.close();
