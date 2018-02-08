1f14de35a41161e793094a22e2762a632c41a103
Jaime <jimmyviniegra@gmail.com>
2018-02-08 20:00:18 +0100

    Pojo Employee

---
+++ src/pojo/Employee.java
@@ -0,0 +1,112 @@
+package pojo;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1163919074127055299L;
+	
+	private Integer id;
+	private String name;
+	private float salary;
+	private Integer phone;
+	private String position;
+	private Integer warehouseId;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(Integer id, String name, float salary, Integer phone, String position, Integer warehouseId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.phone = phone;
+		this.position = position;
+		this.warehouseId = warehouseId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", phone=" + phone + ", position="
+				+ position + ", warehouseId=" + warehouseId + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public Integer getWarehouseId() {
+		return warehouseId;
+	}
+
+	public void setWarehouseId(Integer warehouseId) {
+		this.warehouseId = warehouseId;
+	}
+
+}
