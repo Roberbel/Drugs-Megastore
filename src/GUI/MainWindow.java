5ba93aecbe375baad6c792605eb070a55ff49a01
Roberbel <robertobelmonte98@gmail.com>
2018-02-13 18:00:11 +0100

    Change to JavaFX

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -1,51 +1,18 @@
 package GUI;
 
-import java.awt.*;
-import javax.swing.*;
+import javafx.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class MainWindow extends JFrame{
-	
-	private Container container;
-	private JPanel centralPanel;
-	private JPanel northPanel;
-	private JPanel southPanel;
-	private JPanel eastPanel; 
-	private JPanel westPanel;
-	
-		
-	public static void main(String[] args) {
-		MainWindow window = new MainWindow();
-		window.setVisible(true);
-
+public class MainWindow extends Application {
+	public static void main (String[] args) {
+		launch(args);
 	}
 	
-	MainWindow(){
-		
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-		setTitle("Drug Megastore - database managment");
-		
-		container = getContentPane();
-		container.setLayout(new BorderLayout());
-		northPanel = new JPanel(new FlowLayout());
-		southPanel = new JPanel(new FlowLayout());
-		westPanel = new JPanel(new FlowLayout());
-		eastPanel = new JPanel(new FlowLayout());
-		centralPanel = new JPanel(new FlowLayout());
-		
-		northPanel.add(new JButton ("North"));
-		southPanel.add(new JButton ("South"));
-		eastPanel.add(new JButton ("East"));
-		westPanel.add(new JButton ("West"));
-		centralPanel.add(new JButton("Centro"));
-		
-		
-		container.add(centralPanel, BorderLayout.CENTER);
-		container.add(northPanel, BorderLayout.NORTH);
-		container.add(southPanel, BorderLayout.SOUTH);
-		container.add(eastPanel, BorderLayout.EAST);
-		container.add(westPanel, BorderLayout.WEST);
-		
-		setSize(1000, 900);
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 	
