8438b7aec03c02542faa036bd3ad0bf10a100373
gonka <30638707+gonkaotic@users.noreply.github.com>
2018-02-14 17:46:13 +0100

    added list component

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -1,19 +1,46 @@
 package GUI;
 
-import javafx.*;
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.application.*;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.*;
+
 
 public class MainWindow extends Application {
+	private Stage stage;
+	private ChoiceBox<String> list;
+	
 	public static void main (String[] args) {
 		launch(args);
 	}
 	
 
+	private void listChanged() {
+		System.out.println(list.getValue());
+		
+	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		stage = primaryStage;
 		
+//===========================================list====================================//
+		list = new ChoiceBox <String>();
+		list.getItems().addAll("Hello1","Hello2","Hello3");
+		list.setOnAction(e -> listChanged());
+		
+//===========================================Table===================================//
+		
+		
+//===========================================Pane====================================//
+		FlowPane pane = new FlowPane();
+		pane.getChildren().add();
+		
+//===========================================Scene===================================//		
+		Scene scene = new Scene(pane, 300, 400);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
 }
