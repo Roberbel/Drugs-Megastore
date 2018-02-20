cfc53de2459dfce400bc5d11746791c47132fccd
gonka <30638707+gonkaotic@users.noreply.github.com>
2018-02-20 23:20:13 +0100

    worked the start method

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -1,10 +1,13 @@
 package GUI;
 
 import javafx.application.*;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.*;
@@ -14,14 +17,46 @@ import pojo.*;
 
 public class MainWindow extends Application {
 	private Stage stage;
-	private ChoiceBox<String> list;
+	
 	private BorderPane mainPane;
+	
 	private BorderPane secondPane;
-	private BorderPane tertPane;
+	private FlowPane secondPaneNorth;
+	private BorderPane secondPaneSouth;
+	private FlowPane secondPaneSouthTextBox;
 	private FlowPane tablesPane;
+	private Button add;
+	private Button del;
+	private ChoiceBox<String> list;
+	
+	private BorderPane tertPane;
+	
 	
 	public static void main (String[] args) {
 		launch(args);
+	}
+	
+	private Node[] getTextBoxesDrug() {
+		int num = 4;
+		TextField name;
+		TextField stock;
+		TextField sellingPrice;
+		TextField activePrinciple;
+		//picture
+		
+		name = new TextField();
+		stock = new TextField();
+		sellingPrice = new TextField();
+		activePrinciple = new TextField();
+		
+		Node[] nodes = new Node[num];
+		nodes [0] = name;
+		nodes [1] = stock;
+		nodes [2] = sellingPrice;
+		nodes [3] = activePrinciple;
+		//nodes [4] = picture;
+		return nodes;
+		
 	}
 	
 
@@ -38,18 +73,32 @@ public class MainWindow extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		stage = primaryStage;
 		
-		
-//===========================================List====================================//
+		mainPane = new BorderPane();
+		secondPane = new BorderPane();
+		secondPaneNorth= new FlowPane();
+		secondPaneSouth= new BorderPane();
+		secondPaneSouthTextBox= new FlowPane();	
+		tablesPane = new FlowPane();
+		add = new Button("Add");
+		del = new Button ("Delete");
 		list = new ChoiceBox <String>();
+		
+		
+		
+//========================Secondary panel North(List)====================================================//
+		
 		list.getItems().addAll("Drugs","Arrivals","Clients","Corridors","Deliveries","Employees","Provider","Warehouse");
 		list.setOnAction(e -> listChanged());
-		
-		
-//===========================================Pane====================================//
-		FlowPane listPane = new FlowPane();
-		listPane.getChildren().add(list);
-		
-		BorderPane secondPane=new BorderPane();
+		secondPaneNorth.getChildren().add(list);
+		secondPane.setTop(secondPaneNorth);
+
+//================Secondary Panel South(Add/del buttons and text box)====================================//
+		FlowPane buttonPane = new FlowPane();
+		buttonPane.getChildren().addAll(add, del);
+		secondPaneSouth.setRight(buttonPane);
+		secondPaneSouthTextBox.getChildren().addAll(getTextBoxesDrug());
+		secondPaneSouth.setCenter(secondPaneSouthTextBox);
+		secondPane.setBottom(secondPaneSouth);
 				
 
 		
@@ -65,9 +114,10 @@ public class MainWindow extends Application {
 		
 		TableView <pojo.Employee> employeeTable=new TableView<pojo.Employee>();
 		employeeTable.getColumns().addAll(name,salary,phone,position,warehouse,picture);
-		mainPane = new BorderPane();
-		mainPane.setCenter(employeeTable);
 		
+		tablesPane.getChildren().add(employeeTable);
+		secondPane.setCenter(tablesPane);
+		mainPane.setCenter(secondPane);
 		
 		
 	
