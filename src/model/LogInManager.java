c1b4c6c9e66f471bfa4ec44d5accb1d3a470c691
Roberbel <robertobelmonte98@gmail.com>
2018-03-06 14:49:36 +0100

    Model package added

---
+++ src/model/LogInManager.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.sql.SQLException;
+
+import DB.SQLManager;
+import pojos.User;
+
+public class LogInManager {
+
+	
+		private User trialUser;
+		private User completeUser;
+		
+		
+		
+		public LogInManager() {
+			super();
+		}
+
+		public LogInManager(User trialUser) {
+			super();
+			this.trialUser=trialUser;
+		}
+
+		public User getTrialUser() {
+			return trialUser;
+		}
+
+		public void setTrialUser(User trialUser) {
+			this.trialUser = trialUser;
+		}
+
+		public User getCompleteUser() {
+			return completeUser;
+		}
+
+		public void setCompleteUser(User completeUser) {
+			this.completeUser = completeUser;
+		}
+		
+		
+		public boolean checkExistance() throws SQLException, ClassNotFoundException{
+			SQLManager.connect("jdbc:sqlite:./db/Drug Megastore Users TEST.db");
+			
+			if(SQLManager.checkUser(trialUser)) {
+				completeUser=SQLManager.extractUserByName(trialUser);
+				SQLManager.disconnect();
+				return true;
+			}else {
+				SQLManager.disconnect();
+				return false;
+			}
+		}
+		
+		public boolean checkPassword () {
+			if(trialUser.getPassword().equals(completeUser.getPassword())) {
+				return true;
+			}else {
+				return false;
+			}
+		}
+		
+}
