2fafd9afb121828798808ae3f8b05f56b724c621
gonka <30638707+gonkaotic@users.noreply.github.com>
2018-04-17 20:32:13 +0200

    Worked on the JPAManager with Creates and Reads

--- src/DB/JPAManager.java
+++ src/DB/JPAManager.java
@@ -5,17 +5,35 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import pojos.Arrival;
+import pojos.Arrives;
+import pojos.Client;
 import pojos.Corridor;
 import pojos.Delivery;
 import pojos.Drug;
+import pojos.Employee;
+import pojos.Packaged;
+import pojos.Provider;
+import pojos.Warehouse;
 
 public class JPAManager {
 
 		private static EntityManager em;
+
+		public static void main(String[] args) {
+			
+			JPAManager.connect();
+			List<Corridor> list = JPAManager.getAllCorridors();
+			for(Corridor d: list) {
+				System.out.println(d.toString());
+				
+			}
+			
+		}
 		
-		public static void getEntityManager(){
+		public static void connect(){
 			
 			em = Persistence.createEntityManagerFactory("company-provider").createEntityManager();
 			em.getTransaction().begin();
@@ -27,14 +45,238 @@ public class JPAManager {
 			em.close();
 			System.out.println("Database connection closed.");
 		}
-		public static void createDrugTable(String name, Integer stock, Integer sellingPrice, String activePrinciple, Corridor corridor, byte[] photo){
+		
+/*
+ * =====================================================================================================
+ * 								READS
+ * =====================================================================================================
+ */
+		
+		public static Arrival searchArrivalById(Integer id) {
 			
-			Drug drug=new Drug(name, stock,sellingPrice,activePrinciple,corridor, photo);
+			Query q1 = em.createNativeQuery("SELECT * FROM arrivals WHERE transaction_id = ?", Arrival.class);
+			q1.setParameter(1, id);
+			return (Arrival) q1.getSingleResult();
+			
+		}
+		
+		public static Client searchClientById(Integer id) {
+			
+			Query q1 = em.createNativeQuery("SELECT * FROM client WHERE id = ?", Client.class);
+			q1.setParameter(1, id);
+			return (Client) q1.getSingleResult();
+			
+		}
+		
+		public static Corridor searchCorridorById(Integer id) {
+			
+			Query q1 = em.createNativeQuery("SELECT * FROM corridor WHERE id = ?", Corridor.class);
+			q1.setParameter(1, id);
+			return (Corridor) q1.getSingleResult();
+			
+		}
+		
+		public static Delivery searchDeliveryById(Integer id) {
+			
+			Query q1 = em.createNativeQuery("SELECT * FROM deliveries WHERE transaction_id = ?", Delivery.class);
+			q1.setParameter(1, id);
+			return (Delivery) q1.getSingleResult();
+			
+		}
+		
+		public static Drug searchDrugById(Integer id) {
+			
+			Query q1 = em.createNativeQuery("SELECT * FROM drug WHERE id = ?", Drug.class);
+			q1.setParameter(1, id);
+			return (Drug) q1.getSingleResult();
+			
+		}
+		
+		public static Employee searchEmployeeById(Integer id) {
+			
+			Query q1 = em.createNativeQuery("SELECT * FROM employee WHERE id = ?", Employee.class);
+			q1.setParameter(1, id);
+			return (Employee) q1.getSingleResult();
+			
+		}
+		
+		public static Provider searchProviderById(Integer id) {
+			
+			Query q1 = em.createNativeQuery("SELECT * FROM provider WHERE id = ?", Provider.class);
+			q1.setParameter(1, id);
+			return (Provider) q1.getSingleResult();
+			
+		}
+		
+		public static Warehouse searchWarehouseById(Integer id) {
+			
+			Query q1 = em.createNativeQuery("SELECT * FROM warehouse WHERE id = ?", Warehouse.class);
+			q1.setParameter(1, id);
+			return (Warehouse) q1.getSingleResult();
+			
+		}
+		
+		/*
+		 *  SELECT EVERYTHING FROM EACH TABLE
+		 */
+		
+		
+		public static List<Arrival> getAllArrivals(){
+			
+			Query q1 = em.createNativeQuery("SELECT * FROM arrivals", Arrival.class);
+			return (List<Arrival>) q1.getResultList();
+			
+		}
+		
+		public static List<Arrives> getAllArrives(){
+			
+			Query q1 = em.createNativeQuery("Select * FROM arrives", Arrives.class);
+			return (List<Arrives>) q1.getResultList();
+			
+		}
+		
+		public static List<Client> getAllClients(){
+			
+			Query q1 = em.createNativeQuery("Select * FROM client", Client.class);
+			return (List<Client>) q1.getResultList();
+			
+		}
+		
+		public static List<Corridor> getAllCorridors(){
+			
+			Query q1 = em.createNativeQuery("Select * FROM corridor", Corridor.class);
+			return (List<Corridor>) q1.getResultList();
+			
+		}
+		
+		public static List<Delivery> getAllDeliveries(){
+			
+			Query q1 = em.createNativeQuery("Select * FROM deliveries", Delivery.class);
+			return (List<Delivery>) q1.getResultList();
+			
+		}
+		
+		public static List<Drug> getAllDrugs(){
+			
+			Query q1 = em.createNativeQuery("Select * FROM drug", Drug.class);
+			return (List<Drug>) q1.getResultList();
+			
+		}
+		
+		public static List<Employee> getAllEmployees(){
+			
+			Query q1 = em.createNativeQuery("Select * FROM employee", Employee.class);
+			return (List<Employee>) q1.getResultList();
+			
+		}
+		
+		public static List<Packaged> getAllPackaged(){
+			
+			Query q1 = em.createNativeQuery("Select * FROM packaged", Packaged.class);
+			return (List<Packaged>) q1.getResultList();
+			
+		}
+		
+		public static List<Provider> getAllProviders(){
+			
+			Query q1 = em.createNativeQuery("Select * FROM provider", Provider.class);
+			return (List<Provider>) q1.getResultList();
+			
+		}
+		
+		public static List<Warehouse> getAllWarehouses(){
+			
+			Query q1 = em.createNativeQuery("Select * FROM warehouse", Warehouse.class);
+			return (List<Warehouse>) q1.getResultList();
+			
+		}
+		
+		
+		
+		
+/*
+ * =====================================================================================================
+ * 								INSERTS
+ * =====================================================================================================
+ */		
+		public static void insertArrival(Arrival arrival) {
+			
+			em.getTransaction().begin();
+			em.persist(arrival);
+			em.getTransaction().commit();
+			
+		}
+		
+		public static void insertArrive(Arrives arrive) {
+			
+			em.getTransaction().begin();
+			em.persist(arrive);
+			em.getTransaction().commit();
+			
+		}
+		
+		public static void insertClient(Client client) {
+			
+			em.getTransaction().begin();
+			em.persist(client);
+			em.getTransaction().commit();
+			
+		}
+		
+		public static void insertCorridor(Corridor corridor) {
+			
+			em.getTransaction().begin();
+			em.persist(corridor);
+			em.getTransaction().commit();
+			
+		}
+		
+		public static void insertDelivery(Delivery delivery){
+			
+			em.getTransaction().begin();
+			em.persist(delivery);
+			em.getTransaction().commit();
+			
+		}
+		
+		public static void insertDrug(Drug drug){
+			
 			em.getTransaction().begin();
 			em.persist(drug);
 			em.getTransaction().commit();
 	
 		}
-
-
+		
+		public static void insertEmployee(Employee employee) {
+			
+			em.getTransaction().begin();
+			em.persist(employee);
+			em.getTransaction().commit();
+			
 		}
+		
+		public static void insertPackaged(Packaged packaged) {
+			
+			em.getTransaction().begin();
+			em.persist(packaged);
+			em.getTransaction().commit();
+			
+		}
+		
+		public static void insertProvider(Provider provider) {
+			
+			em.getTransaction().begin();
+			em.persist(provider);
+			em.getTransaction().commit();
+			
+		}
+		
+		public static void insertWarehouse(Warehouse warehouse) {
+			
+			em.getTransaction().begin();
+			em.persist(warehouse);
+			em.getTransaction().commit();
+			
+		}
+
+}
