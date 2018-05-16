c43ece45c6262274e76fe2d309f008f315ea8fac
gonka <30638707+gonkaotic@users.noreply.github.com>
2018-05-16 21:49:40 +0200

    deleted unnecesary classes in gui.clientPanel

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -8,7 +8,6 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 
 import gui.adminPanel.AdminWindow;
-import gui.clientPanel.ClientPane;
 import gui.clientPanel.ClientPanelSB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
