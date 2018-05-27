f8096d3204bd5f9fbe07899d8241b9e8a547c7f2
Jaime <jimmyviniegra@gmail.com>
2018-05-27 18:56:19 +0200

    GUI changes. We dont forget the LogInManager. RIP

--- src/gui/employeePanel/AddArrivalPane.java
+++ src/gui/employeePanel/AddArrivalPane.java
@@ -17,11 +17,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
@@ -34,7 +36,6 @@ import pojos.Provider;
 
 public class AddArrivalPane implements Initializable {
 
-	
 	@FXML
 	private ComboBox<Provider> providerComboBox;
 
@@ -64,7 +65,7 @@ public class AddArrivalPane implements Initializable {
 
 	@FXML
 	private JFXButton addArrivalButton;
-	
+
 	@FXML
 	private JFXButton newGroupButton;
 
@@ -81,84 +82,104 @@ public class AddArrivalPane implements Initializable {
 			yesCheckBox.setSelected(false);
 		}
 	}
-	
+
 	@FXML
 	void createArrives(MouseEvent event) {
-		
+
 		Arrives createdArrives = new Arrives();
-		
-		createdArrives.setDrug(drugPicker.getSelectionModel().getSelectedItem());
-		createdArrives.setAmount(Integer.parseInt(stockPicker.getText()));
-		newInventoryTable.getItems().add(createdArrives);
-		
-		drugPicker.getSelectionModel().clearSelection();
-		stockPicker.clear();
+
+		try {
+			if (drugPicker.getSelectionModel().getSelectedItem() != null) {
+				createdArrives.setDrug(drugPicker.getSelectionModel().getSelectedItem());
+			} else {
+				throw new NumberFormatException();
+			}
+			createdArrives.setAmount(Integer.parseInt(stockPicker.getText()));
+			newInventoryTable.getItems().add(createdArrives);
+
+			drugPicker.getSelectionModel().clearSelection();
+			stockPicker.clear();
+		} catch (NumberFormatException ex) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Wrong data type");
+			alert.setContentText("Please select a drug and type a number");
+
+			alert.show();
+		}
+
 	}
-	
+
 	@FXML
-	void newArrival (MouseEvent event) {
-	    Arrival toBeAdded = new Arrival();
-	    
-	    toBeAdded.setProvider(providerComboBox.getSelectionModel().getSelectedItem());
-	    toBeAdded.setDate(java.sql.Date.valueOf(dateNewArrival.getValue()));
-	    if(yesCheckBox.isSelected()) {
-	    		toBeAdded.setReceived(true);
-	    }else {
-	    		toBeAdded.setReceived(false);
-	    }
-		
-	    //JPAManager.connect();
-	    JPAManager.insertArrival(toBeAdded);
-	    //JPAManager.disconnect();
-	    
-	   
-	    
-	    for (Arrives item : newInventoryTable.getItems()) {
-	        Drug droga = newDrugs.getCellObservableValue(item).getValue();
-	        Integer stock = newStocks.getCellObservableValue(item).getValue();
-	        Arrives arrive = new Arrives(droga.getId(),toBeAdded.getArrivalId(),stock);
-	        arrive.setDrug(droga);
-	        
-	        try {
-				SQLManager.insertArrives(arrive);
-			} catch (SQLException e) {
+	void newArrival(MouseEvent event) {
+		Arrival toBeAdded = new Arrival();
+
+		try {
+			if(providerComboBox.getSelectionModel().getSelectedItem()!=null) {	
+			toBeAdded.setProvider(providerComboBox.getSelectionModel().getSelectedItem());
+			}else {
+				throw new NullPointerException();
+			}
+			toBeAdded.setDate(java.sql.Date.valueOf(dateNewArrival.getValue()));
+			if (yesCheckBox.isSelected()) {
+				toBeAdded.setReceived(true);
+			} else if(noCheckBox.isSelected()){
+				toBeAdded.setReceived(false);
+			} else {
+				throw new NullPointerException();
+			}
+
+			// JPAManager.connect();
+			JPAManager.insertArrival(toBeAdded);
+			// JPAManager.disconnect();
+
+			for (Arrives item : newInventoryTable.getItems()) {
+				Drug droga = newDrugs.getCellObservableValue(item).getValue();
+				Integer stock = newStocks.getCellObservableValue(item).getValue();
+				Arrives arrive = new Arrives(droga.getId(), toBeAdded.getArrivalId(), stock);
+				arrive.setDrug(droga);
+
+				try {
+					SQLManager.insertArrives(arrive);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/employeePanel/employeeWindow.fxml"));
+				BorderPane root = loader.load();
+				EmployeeWindow controller = loader.<EmployeeWindow>getController();
+				Scene scene = addArrivalButton.getScene();
+				Stage stage = (Stage) scene.getWindow();
+				stage.setScene(new Scene(root));
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	    }
-	    
-	    
-	    try {
-	    		FXMLLoader loader=new FXMLLoader(getClass().getResource("/gui/employeePanel/employeeWindow.fxml"));
-			BorderPane root = loader.load();
-			EmployeeWindow controller = loader.<EmployeeWindow>getController();
-			Scene scene = addArrivalButton.getScene();
-			Stage stage = (Stage) scene.getWindow();
-			stage.setScene(new Scene(root));
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (NullPointerException ex) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Empty fields");
+			alert.setContentText("Please fill all the fields");
+
+			alert.show();
+
 		}
-	    
-	    
-	   
-}
-	    
-		
-	
+
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		try {
 			providerComboBox.getItems().addAll(SQLManager.getAllProviders());
 			drugPicker.getItems().addAll(SQLManager.getAllDrugs());
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		newDrugs.setCellValueFactory(new PropertyValueFactory <Arrives,Drug>("drug"));
-		newStocks.setCellValueFactory(new PropertyValueFactory <Arrives,Integer>("amount"));
+
+		newDrugs.setCellValueFactory(new PropertyValueFactory<Arrives, Drug>("drug"));
+		newStocks.setCellValueFactory(new PropertyValueFactory<Arrives, Integer>("amount"));
 	}
 
 }
--- src/gui/employeePanel/AddDeliveryPane.java
+++ src/gui/employeePanel/AddDeliveryPane.java
@@ -15,11 +15,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
@@ -33,124 +35,147 @@ import pojos.Packaged;
 
 public class AddDeliveryPane implements Initializable {
 
-    @FXML
-    private ComboBox<Client> clientComboBox;
+	@FXML
+	private ComboBox<Client> clientComboBox;
 
-    @FXML
-    private DatePicker dateNewArrival;
+	@FXML
+	private DatePicker dateNewArrival;
 
-    @FXML
-    private JFXCheckBox yesCheckBox;
+	@FXML
+	private JFXCheckBox yesCheckBox;
 
-    @FXML
-    private JFXCheckBox noCheckBox;
+	@FXML
+	private JFXCheckBox noCheckBox;
 
-    @FXML
-    private TableView<Packaged> newInventoryTable;
+	@FXML
+	private TableView<Packaged> newInventoryTable;
 
-    @FXML
-    private TableColumn<Packaged, Drug> newDrugs;
+	@FXML
+	private TableColumn<Packaged, Drug> newDrugs;
 
-    @FXML
-    private TableColumn<Packaged, Integer> newStocks;
+	@FXML
+	private TableColumn<Packaged, Integer> newStocks;
 
-    @FXML
-    private ComboBox<Drug> drugPicker;
+	@FXML
+	private ComboBox<Drug> drugPicker;
 
-    @FXML
-    private JFXButton newGroupButtom;
+	@FXML
+	private JFXButton newGroupButtom;
 
-    @FXML
-    private JFXButton addDeliveryButton;
+	@FXML
+	private JFXButton addDeliveryButton;
 
-    @FXML
-    private TextField stockPicker;
+	@FXML
+	private TextField stockPicker;
 
-    @FXML
-    void createsPackaged(MouseEvent event) {
-    		Packaged createdPackages = new Packaged();
-		
-		createdPackages.setDrug(drugPicker.getSelectionModel().getSelectedItem());
-		createdPackages.setAmount(Integer.parseInt(stockPicker.getText()));
-		newInventoryTable.getItems().add(createdPackages);
-		
-		drugPicker.getSelectionModel().clearSelection();
-		stockPicker.clear();
-    
-    }
+	@FXML
+	void createsPackaged(MouseEvent event) {
+		Packaged createdPackages = new Packaged();
 
-    @FXML
-    void handleNoCheckBox() {
-    	if (noCheckBox.isSelected()) {
+		try {
+			if (drugPicker.getSelectionModel().getSelectedItem() != null) {
+				createdPackages.setDrug(drugPicker.getSelectionModel().getSelectedItem());
+			} else {
+				throw new NumberFormatException();
+			}
+			createdPackages.setAmount(Integer.parseInt(stockPicker.getText()));
+			newInventoryTable.getItems().add(createdPackages);
+
+			drugPicker.getSelectionModel().clearSelection();
+			stockPicker.clear();
+		} catch (NumberFormatException ex) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Wrong data type");
+			alert.setContentText("Please select a drug and type a number");
+
+			alert.show();
+		}
+	}
+
+	@FXML
+	void handleNoCheckBox() {
+		if (noCheckBox.isSelected()) {
 			yesCheckBox.setSelected(false);
 		}
-    }
+	}
 
-    @FXML
-    void handleYesCheckBox() {
-    	if (yesCheckBox.isSelected()) {
+	@FXML
+	void handleYesCheckBox() {
+		if (yesCheckBox.isSelected()) {
 			noCheckBox.setSelected(false);
 		}
-    }
+	}
 
-    @FXML
-    void newDelivery(MouseEvent event) {
-    		Delivery toBeAdded = new Delivery();
-	    
-	    toBeAdded.setClient(clientComboBox.getSelectionModel().getSelectedItem());
-	    toBeAdded.setTransactionDate((java.sql.Date.valueOf(dateNewArrival.getValue())));
-	    if(yesCheckBox.isSelected()) {
-	    		toBeAdded.setSent(true);
-	    }else {
-	    		toBeAdded.setSent(false);
-	    }
-		
-	    //JPAManager.connect();
-	    JPAManager.insertDelivery(toBeAdded);
-	    //JPAManager.disconnect();
-	    
-	  
-	    
-	    for (Packaged item : newInventoryTable.getItems()) {
-	        Drug droga = newDrugs.getCellObservableValue(item).getValue();
-	        Integer stock = newStocks.getCellObservableValue(item).getValue();
-	        Packaged packages = new Packaged(droga.getId(),toBeAdded.getTransactionId(),stock);
-	        packages.setDrug(droga);
-	        try {
-				SQLManager.insertPackaged(packages);
-			} catch (SQLException e) {
+	@FXML
+	void newDelivery(MouseEvent event) {
+		Delivery toBeAdded = new Delivery();
+		try {
+			if (clientComboBox.getSelectionModel().getSelectedItem() != null) {
+				toBeAdded.setClient(clientComboBox.getSelectionModel().getSelectedItem());
+			} else {
+				throw new NullPointerException();
+			}
+			toBeAdded.setTransactionDate((java.sql.Date.valueOf(dateNewArrival.getValue())));
+			if (yesCheckBox.isSelected()) {
+				toBeAdded.setSent(true);
+			} else if(noCheckBox.isSelected()){
+				toBeAdded.setSent(false);
+			} else {
+				throw new NullPointerException();
+			}
+
+			// JPAManager.connect();
+			JPAManager.insertDelivery(toBeAdded);
+			// JPAManager.disconnect();
+
+			for (Packaged item : newInventoryTable.getItems()) {
+				Drug droga = newDrugs.getCellObservableValue(item).getValue();
+				Integer stock = newStocks.getCellObservableValue(item).getValue();
+				Packaged packages = new Packaged(droga.getId(), toBeAdded.getTransactionId(), stock);
+				packages.setDrug(droga);
+				try {
+					SQLManager.insertPackaged(packages);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/employeePanel/employeeWindow.fxml"));
+				BorderPane root = loader.load();
+				EmployeeWindow controller = loader.<EmployeeWindow>getController();
+				Scene scene = addDeliveryButton.getScene();
+				Stage stage = (Stage) scene.getWindow();
+				stage.setScene(new Scene(root));
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	    }
-	    
-	    try {
-    		FXMLLoader loader=new FXMLLoader(getClass().getResource("/gui/employeePanel/employeeWindow.fxml"));
-		BorderPane root = loader.load();
-		EmployeeWindow controller = loader.<EmployeeWindow>getController();
-		Scene scene = addDeliveryButton.getScene();
-		Stage stage = (Stage) scene.getWindow();
-		stage.setScene(new Scene(root));
-		
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		} catch (NullPointerException ex) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Empty fields");
+			alert.setContentText("Please fill all the fields");
+
+			alert.show();
+
+		}
+
 	}
-    
-    }
-    
-    @Override
+
+	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		//try {
-			//JPAManager.connect();
-			clientComboBox.getItems().addAll(JPAManager.getAllClients());
-			drugPicker.getItems().addAll(JPAManager.getAllDrugs());
-			
-		//} catch (SQLException e) {
-			//e.printStackTrace();
-		//}
-		
-		newDrugs.setCellValueFactory(new PropertyValueFactory <Packaged,Drug>("drug"));
-		newStocks.setCellValueFactory(new PropertyValueFactory <Packaged,Integer>("amount"));
+		// try {
+		// JPAManager.connect();
+		clientComboBox.getItems().addAll(JPAManager.getAllClients());
+		drugPicker.getItems().addAll(JPAManager.getAllDrugs());
+
+		// } catch (SQLException e) {
+		// e.printStackTrace();
+		// }
+
+		newDrugs.setCellValueFactory(new PropertyValueFactory<Packaged, Drug>("drug"));
+		newStocks.setCellValueFactory(new PropertyValueFactory<Packaged, Integer>("amount"));
 	}
 
 }
